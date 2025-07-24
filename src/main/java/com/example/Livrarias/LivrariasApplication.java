4329e037895c237e236da764cc9fc9811bab9003
Renato <renatoconrado38@gmail.com>
2025-07-24 18:54:29 -0300

    Corrigido

--- src/main/java/com/example/Livrarias/LivrariasApplication.java
+++ src/main/java/com/example/Livrarias/LivrariasApplication.java
@@ -1,24 +1,15 @@
 package com.example.Livrarias;
 
-import java.time.LocalDate;
-
-
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import com.example.Livrarias.model.autor.Autor;
-import com.example.Livrarias.repository.AutorRepository;
 
 @SpringBootApplication
 public class LivrariasApplication {
 
-	public static void main(String[] args) {
-		 var context  = SpringApplication.run(LivrariasApplication.class, args);
-  //   AutorRepository repository = context.getBean(AutorRepository.class);
- //salvarRegistro(repository);
- 
-	}
-	}
-	
+    public static void main(String[] args) {
+        var context = SpringApplication.run(LivrariasApplication.class, args);
+//        AutorRepository repository = context.getBean(AutorRepository.class);
+//        salvarRegistro(repository);
+
+    }
+}
--- src/main/java/com/example/Livrarias/repository/AutorRepository.java
+++ src/main/java/com/example/Livrarias/repository/AutorRepository.java
@@ -1,15 +1,12 @@
 package com.example.Livrarias.repository;
 
-import java.util.UUID;
-
-import org.springframework.boot.context.properties.bind.Name;
+import com.example.Livrarias.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import com.example.Livrarias.model.autor.Autor;
+import java.util.UUID;
 
 @Repository
-public interface AutorRepository extends JpaRepository<Autor, UUID>{
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
 }
